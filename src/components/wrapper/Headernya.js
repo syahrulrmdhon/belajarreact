298cb3eb605c3bb62fccc7c5ecf2ddee408f770f
syahrulrmdhon <syahrulrmdhon2@gmail.com>
2017-11-29 19:20:50 +0700

    dirapihkan

--- src/components/wrapper/Headernya.js
+++ src/components/wrapper/Headernya.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Menu,Input} from 'semantic-ui-react';
+import {Menu,Input,Visibility} from 'semantic-ui-react';
 
+const FixedMenu = () => (
+  <Menu fixed='top' size='large'>
+    <Menu.Item name='beranda'
+    />
+    <Menu.Item name='belajar react'
+    />
+    <Menu.Menu position='right'>
+      <Menu.Item>
+         <Input icon='search' placeholder='Search...' />
+      </Menu.Item>
+      <Menu.Item>
+        <Link to='/login'>Login</Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to='/signup'>Daftar</Link>
+      </Menu.Item>
+    </Menu.Menu>
+   </Menu>
+)
 
 class Headernya extends React.Component {
   state = {
     activeItem: 'beranda',
  }
- handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  hideFixedMenu = () => this.setState({ visible: false })
+  showFixedMenu = () => this.setState({ visible: true })
   render() {
-    const { activeItem } = this.state;
+    const { activeItem,visible } = this.state;
     return(
+      <div>
+      { visible ? <FixedMenu /> : null }
+
+        <Visibility
+          onBottomPassed={this.showFixedMenu}
+          onBottomVisible={this.hideFixedMenu}
+          once={false}
+        >
       <Menu pointing secondary size='large'>
         <Menu.Item name='beranda'
           active={activeItem === 'beranda'}
@@ -32,7 +61,8 @@ class Headernya extends React.Component {
           </Menu.Item>
         </Menu.Menu>
        </Menu>
-
+       </Visibility>
+       </div>
       );
   }
 }
