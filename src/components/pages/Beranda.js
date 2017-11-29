f052e7d43bdb3ed8f58d867fa92e90f93a56bba0
syahrulrmdhon <syahrulrmdhon2@gmail.com>
2017-11-29 16:01:56 +0700

    ubah beranda

--- src/components/pages/Beranda.js
+++ src/components/pages/Beranda.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import {Link,browserHistory} from 'react-router-dom';
-import {Menu,Input} from 'semantic-ui-react';
+import Headernya from '../wrapper/Headernya';
+
 
 class Beranda extends React.Component {
-  state = {
-    activeItem: 'beranda',
- }
-
- handleItemClick = (e, { name }) => this.setState({ activeItem: name })
   render() {
-    const { activeItem } = this.state;
+
     return(
-    <Menu secondary>
-        <Menu.Item name='beranda' active={activeItem === 'beranda'} onClick={this.handleItemClick} />
-        <Menu.Item name='belajar' active={activeItem === 'belajar'} onClick={this.handleItemClick} />
-        <Menu.Menu position='right'>
-          <Menu.Item>
-            <Input icon='search' placeholder='Search...' />
-          </Menu.Item>
-          <Menu.Item>
-            <Link to='/login'>Login</Link>
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
+      <div>
+        <Headernya/>
+      </div>
       );
   }
 }
