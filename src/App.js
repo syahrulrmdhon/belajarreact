c6a5f0b047543d7c1c66f2f85f6565456cbb8d90
syahrulrmdhon <syahrulrmdhon2@gmail.com>
2017-11-29 19:21:31 +0700

    ilangin container

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Route} from 'react-router-dom';
 import Beranda from './components/pages/Beranda';
 import HalamanLogin from './components/pages/HalamanLogin';
 
-const App = () => <div className='ui container'>
+const App = () => <div>
     <Route path='/' exact component={Beranda}/>
     <Route path='/login' exact component={HalamanLogin}/>
 </div>
