9bd1cec89fd1841875bda5ef0dc0b33192a6b0e3
syahrulrmdhon <syahrulrmdhon2@gmail.com>
2017-11-29 19:18:30 +0700

    dirapikan + tambah warna

--- src/components/pages/Beranda.js
+++ src/components/pages/Beranda.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Headernya from '../wrapper/Headernya';
+import Footer from '../wrapper/Footer';
+import {Segment, Container, Header, Icon, Button,Image} from 'semantic-ui-react';
 
 
 class Beranda extends React.Component {
@@ -8,6 +10,36 @@ class Beranda extends React.Component {
     return(
       <div>
         <Headernya/>
+        <Segment
+            inverted
+            textAlign='center'
+            style={{
+              minHeight: 700,
+              padding: '1em 0em'
+             }}
+            vertical
+            color="red"
+          >
+        <Container text>
+              <Header
+                as='h1'
+                content='BelajarReact.com'
+                inverted
+                style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
+              />
+              <Header
+                as='h2'
+                content='BelajarReact cepat dan tepat'
+                inverted
+                style={{ fontSize: '1.7em', fontWeight: 'normal' }}
+              />
+              <Button primary size='huge'>
+                Ayo Mulai
+                <Icon name='right arrow' />
+              </Button>
+            </Container>
+          </Segment>
+        <Footer/>
       </div>
       );
   }
